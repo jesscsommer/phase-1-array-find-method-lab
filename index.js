7119f3cb284061ec01f5affe9cd19bbd652db36c
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-04-14 10:18:20 -0700

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function isWin(record){
+    return record.result === 'W';
+  }
+  
+  function superbowlWin(record){
+    record.find(isWin);
+    if (record.find(isWin)) {
+        return (record.find(isWin).year)
+    }
+  }
